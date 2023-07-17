20569e7dc9bacbff1127493ddbec3e052b61a425
lin121291 <4jp33f9e@gmail.com>
2023-07-17 10:47:19 +0800

    fix: complete function send_log_to_GA send data

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
+#-*-coding:UTF-8-*-
 import random
 import logging
-import google.cloud.logging
-from flask import Flask, request, send_file, jsonify, abort
-import uuid
 import requests
 import json
+import google.cloud.logging
+from flask import Flask, request, send_file, jsonify, abort
 from PIL import Image
 
 app = Flask(__name__)
-GA_url = f'https://www.google-analytics.com/mp/collect?measurement_id=G-46978BE92P&api_secret=BcsrljfJTnqTGXndMPVgyA'
+GA_url = 'https://www.google-analytics.com/mp/collect?measurement_id=G-46978BE92P&api_secret=BcsrljfJTnqTGXndMPVgyA'
 
-#log config 放上GCP拿掉
+#google cloud log config - local test don't need
 """
 client = google.cloud.logging.Client()
 client.setup_logging()
@@ -23,23 +23,22 @@ logging.basicConfig(
 )
 
 
-def send_log＿to_GA(err):
+def send_log_to_GA(err):
     try:
         payload = {
-            'client_id': '111.111',
+            'client_id': 'junis',
             'events': [{
-                'name': 'yoooooo',
+                'name': 'log',
                 'params': {
-                    'ok':'true'
+                    'log': err
                 },
             }]
         }
         headers = {'Content-type': 'application/json'}
-        r=requests.post(GA_url,
-                      headers=headers,
-                      data=json.dumps(payload),
-                      verify=True)
-        logging.warning(r)
+        r = requests.post(GA_url,
+                          headers=headers,
+                          data=json.dumps(payload),
+                          verify=True)
     except requests.exceptions.RequestException as e:
         logging.warning(e)
 
@@ -52,7 +51,9 @@ def generate_image(width, height):
         image.save(temp_filename, 'PNG')
         return send_file(temp_filename, mimetype='image/png')
     except:
-        logging.ERROR("server error")
+        err = "server generate image fail"
+        logging.ERROR(err)
+        send_log_to_GA(err)
         content = {'error': "server error"}
         return jsonify(content), 500
 
@@ -70,8 +71,10 @@ def generate_image_route():
             content = {'data-error': err}
             return jsonify(content), 400
     else:
-        logging.warning("request data not in JSON format")
-        content = {'data-error': "request data should be JSON"}
+        err = "request data not JSON format"
+        logging.warning(err)
+        send_log_to_GA(err)
+        content = {'data-error': err}
         return jsonify(content), 400
 
     return generate_image(width, height)
